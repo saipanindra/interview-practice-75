863e7671fe44816a7ff77fce1da3e11aacf73bc4
Sai Paleti <saipaleti@Sais-MacBook-Pro.local>
2025-12-23 22:06:38 -0800

    Binary Search

---
+++ BinarySearchTester.java
@@ -0,0 +1,8 @@
+public class BinarySearchTester {
+    public static void main(String[] args) {
+        BinarySearch b = new BinarySearch();
+        System.out.println(b.search(new int[] {-1,0,2,4,6,8}, 3));
+        
+    }
+    
+}
